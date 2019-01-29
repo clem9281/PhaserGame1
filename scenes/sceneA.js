d9d879bb6860aa27f193678631b8bb6104234fe9
clem9281 <clem9281@gmail.com>
2019-01-28 20:42:23 -0700

    added camera and movement logic

--- scenes/sceneA.js
+++ scenes/sceneA.js
@@ -5,7 +5,6 @@ class SceneA extends Phaser.Scene {
     super({ key: "SceneA", active: true });
   }
   preload() {
-    this.load.image("stars", "assets/spaceparts/stars.png");
     this.load.image("ship1", "assets/sprites/ship1.png");
     this.load.image("cursor", "assets/sprites/crosshairs.png");
     this.load.image("startiles", "assets/spaceparts/tiles/starTiles.png");
@@ -16,7 +15,7 @@ class SceneA extends Phaser.Scene {
     );
   }
   create() {
-    console.log(this.cache.tilemap.get("map").data);
+    // maps
     let map = this.add.tilemap("map");
     let startiles = map.addTilesetImage("starTiles", "startiles");
     let starLayer = map.createStaticLayer("sky", startiles, 0, 0);
@@ -31,40 +30,74 @@ class SceneA extends Phaser.Scene {
     let iceplanetLayer = map.createStaticLayer("iceplanet", planettiles, 0, 0);
     let gasgiantLayer = map.createStaticLayer("gasgiant", planettiles, 0, 0);
     let exoplanetLayer = map.createStaticLayer("exoplanet", planettiles, 0, 0);
+
+    // world bounds
+    this.physics.world.bounds.width = starLayer.width;
+    this.physics.world.bounds.height = starLayer.height;
+
+    // cursor
     this.cursor = this.add.image(0, 0, "cursor").setVisible(false);
+
+    // player
     player = this.physics.add
       .sprite(
-        // window.innerWidth / 2,
-        // window.innerHeight / 2.5,
         this.physics.world.bounds.centerX,
         this.physics.world.bounds.centerY,
         "ship1"
       )
       .setVelocity(0, 0);
     player.body.onOverlap = true;
-    // .setAngularDrag(40)
-    // .setAngularVelocity(-40);
-    console.log(player.body);
     player.body.setCollideWorldBounds(true);
-    console.log(player.rotation); // cursors = this.input.keyboard.createCursorKeys();
-    // player.body.setCollideWorldBounds(true);
-    // player.body.setVelocity(0, 0);
+    console.log(this);
+    console.log("body", player);
+
+    // camera
+    this.cameras.main.startFollow(player);
+    this.cameras.main.setBounds(0, 0, starLayer.width, starLayer.height);
+    console.log("camera", this.cameras);
+    console.log(this.physics);
+    console.log("cursor", this.cursor);
   }
 
   update() {
     if (this.input.activePointer.isDown) {
-      console.log(this.input.activePointer);
-      this.cursor
-        .setVisible(true)
-        .setPosition(this.input.activePointer.x, this.input.activePointer.y);
-      this.physics.moveToObject(player, this.input.activePointer, 700);
+      this.cursor.setVisible(true);
+      // movement
+      if (this.input.activePointer.x > 480) {
+        this.cursor.setX(
+          this.cameras.main.midPoint.x + this.input.activePointer.x - 480
+        );
+      } else if (this.input.activePointer.x < 480) {
+        this.cursor.setX(
+          this.cameras.main.midPoint.x - (480 - this.input.activePointer.x)
+        );
+      }
+      if (this.input.activePointer.y < 640) {
+        this.cursor.setY(
+          this.cameras.main.midPoint.y - (640 - this.input.activePointer.y)
+        );
+      } else if (this.input.activePointer.y > 640) {
+        this.cursor.setY(
+          this.cameras.main.midPoint.y + this.input.activePointer.y - 640
+        );
+      }
 
+      console.log("cameras", this.cameras.main.midPoint);
+      console.log(
+        "pointer",
+        this.input.activePointer.x,
+        this.input.activePointer.y
+      );
+      console.log("cursor", this.cursor.x, this.cursor.y);
+      console.log("ship", player.x, player.y);
+      console.log("body", player.body.x, player.body.y);
+      this.physics.moveToObject(player, this.cursor, 700);
       player.rotation =
         Phaser.Math.Angle.Between(
           player.x,
           player.y,
-          this.input.activePointer.x,
-          this.input.activePointer.y
+          this.cursor.x,
+          this.cursor.y
         ) + 1.57;
       let lastDirection =
         Phaser.Math.Angle.Between(
@@ -73,6 +106,7 @@ class SceneA extends Phaser.Scene {
           this.input.activePointer.x,
           this.input.activePointer.y
         ) + 1.57;
+      // not sure yet if this is working
       if (
         player.x === this.input.activePointer.x ||
         player.y === this.input.activePointer.y
